14e9352880cf4e35883bdbb01037dc2a154bb50f
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-07-17 13:06:21 +0200

    fix lint

--- platzky/__init__.py
+++ platzky/__init__.py
@@ -1,1 +1,3 @@
-from .platzky import create_app_from_config, create_engine, Engine
+from .platzky import create_app_from_config as create_app_from_config
+from .platzky import create_engine as create_engine
+from .platzky import Engine as Engine
