f9236704b893935d7b62cfa1af97ac7ab3039d77
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-08-19 12:31:14 +0200

    added some tests to models

--- tests/unit_tests/test_models.py
+++ tests/unit_tests/test_models.py
@@ -1,6 +1,7 @@
 from platzky.models import Post, Color, Image
 import pytest
 
+
 def test_posts_are_sorted_by_date():
     older_post = Post(
         author="author",
@@ -31,7 +32,24 @@ def test_posts_are_sorted_by_date():
     assert older_post < newer_post
 
 
-# create test which check if color class can have values only between 0 and 255
+def test_that_posts_cant_be_compared_with_other_types():
+    post = Post(
+        author="author",
+        slug="slug",
+        title="title",
+        contentInMarkdown="content",
+        comments=[],
+        excerpt="excerpt",
+        tags=[],
+        language="en",
+        coverImage=Image(),
+        date="2021-02-19",
+    )
+
+    with pytest.raises(NotImplementedError):
+        _ = post < 1
+
+
 def test_color_values():
     with pytest.raises(ValueError):
         Color(r=256, g=0, b=0, a=0)
@@ -44,3 +62,5 @@ def test_color_values():
 
     with pytest.raises(ValueError):
         Color(r=0, g=0, b=0, a=256)
+
+    _ = Color(r=10, g=200, b=50, a=250)
