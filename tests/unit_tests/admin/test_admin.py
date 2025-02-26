0bd9963066adc5eef669f951dd28a6cd0ff1a70b
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2025-02-26 08:47:14 +0100

    fixed lint

--- tests/unit_tests/admin/test_admin.py
+++ tests/unit_tests/admin/test_admin.py
@@ -1,7 +1,10 @@
 from unittest.mock import Mock, patch
+
 import pytest
 from flask import Flask, session
+
 from platzky.admin.admin import create_admin_blueprint
+
 
 @pytest.fixture
 def admin_blueprint():
@@ -13,25 +16,33 @@ def admin_blueprint():
     app.register_blueprint(blueprint)
     app.mock_db = mock_db
     app.login_methods = mock_login_methods
-    app.secret_key = 'test_secret_key'
+    app.secret_key = "test_secret_key"
     return app
+
 
 @patch("platzky.admin.admin.render_template")
 def test_admin_panel_renders_login_when_no_user(mock_render_template, admin_blueprint):
     with admin_blueprint.test_request_context("/admin/"):
         session["user"] = None
         admin_blueprint.view_functions["admin.admin_panel"]()
-        mock_render_template.assert_called_with("login.html", login_methods=admin_blueprint.login_methods)
+        mock_render_template.assert_called_with(
+            "login.html", login_methods=admin_blueprint.login_methods
+        )
+
 
 @patch("platzky.admin.admin.render_template")
 def test_admin_panel_renders_admin_when_user_exists(mock_render_template, admin_blueprint):
-    admin_blueprint.mock_db.get_plugins_data.return_value = [{"name": "plugin1"}, {"name": "plugin2"}]
+    admin_blueprint.mock_db.get_plugins_data.return_value = [
+        {"name": "plugin1"},
+        {"name": "plugin2"},
+    ]
     with admin_blueprint.test_request_context("/admin/"):
         session["user"] = "test_user"
         admin_blueprint.view_functions["admin.admin_panel"]()
         mock_render_template.assert_called_with(
             "admin.html", user="test_user", cms_modules={"plugins": ["plugin1", "plugin2"]}
         )
+
 
 @patch("platzky.admin.admin.render_template")
 def test_admin_panel_handles_empty_plugins_data(mock_render_template, admin_blueprint):
