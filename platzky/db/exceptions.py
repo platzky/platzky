6aa91deb89233822a7cbdef29e34b814a9d0b014
Krzysztof Kołodziński <krzysztof.kolodzinski@problematy.pl>
2026-07-04 20:49:24 +0200

    added missing file

---
+++ platzky/db/exceptions.py
@@ -0,0 +1,13 @@
+"""Exceptions raised by database implementations."""
+
+
+class DBError(Exception):
+    """Base class for all database errors."""
+
+
+class NotFoundError(DBError, ValueError):
+    """Requested content does not exist in the database.
+
+    Inherits from ValueError for backward compatibility with code that
+    catches ValueError; that base will be dropped in the next major release.
+    """
